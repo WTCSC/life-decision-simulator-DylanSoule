2047d3ce956cd538b59d3be56bb9301da4e0942d
Quarqusart <dylan.soule@icloud.com>
2025-09-12 12:57:46 -0600

    Fixed more spelling errors

--- decision_tree.py
+++ decision_tree.py
@@ -13,19 +13,19 @@ if input("Welcome to your new life, to begin you get home from school and are co
             else:
                 nobankrob = True
         else:
-            print("You're so honorable, with this goodness in you, you end up becoming a wealthy charatitable man, and live happily and die with a good legacy.")
+            print("You're so honorable, with this goodness in you, you end up becoming a wealthy charitable man, and live happily and die with a good legacy.")
     elif not(math_hw or english_hw):
         print("Luckily for you school gets canceled the next day.")
         input("Do you do your homework now?(yes/no)\n")
-        print("Russia bombs the United States becuase you didn't solve the math problems quick enough to help the government protect the country. The world goes into nuclear fallout.")
+        print("Russia bombs the United States because you didn't solve the math problems quick enough to help the government protect the country. The world goes into nuclear fallout.")
     elif math_hw and not(english_hw):
         print("Your essay was due two days ago, when you go into school your teacher gets really mad.")
         if input("Do you apologize?(yes/no)\n").lower() == "yes":
-            print("Your teacher reveales themselves to be Zues, and becuase you were nice to him, you get to become the god of math homework, and live in eternal joy as your sadistic self watches highschool kids struggle with their homework.")
+            print("Your teacher reveals themselves to be Zues, and because you were nice to him, you get to become the god of math homework, and live in eternal joy as your sadistic self watches highschool kids struggle with their homework.")
         else:
             terminating = True
     elif not(math_hw) and english_hw:
-        print("Becuase of this one homework assignment you decide to become and english major, unfortunately becuase of this you are now homeless.")
+        print("Because of this one homework assignment you decide to become and english major, unfortunately because of this you are now homeless.")
         if input("Do you rob a bank to get some money?(yes/no)\n").lower() == "yes":
             bankrob = True
         else:
@@ -38,9 +38,9 @@ else:
             print("He tells you to get him whatever.")
             tea = input("Do you get earl grey tea, black tea, or green tea for him?(earl grey/black/green)\n").lower()
             if tea == "earl grey":
-                print("Arnold loves your choice, and because of that, he decides not to kill you. However, becuase of this you end up starting the robot war and the earth is nuked and split into millions of pieces.")
+                print("Arnold loves your choice, and because of that, he decides not to kill you. However, because of this you end up starting the robot war and the earth is nuked and split into millions of pieces.")
             elif tea == "black":
-                print("Arnold doesn't like your choice, but he respects it. He takes you to the future and you end up becoming a general that figthts robots. But they don't exist becuase you didn't invent them. You are then stuck in a paradox and live in immesurable pain.")
+                print("Arnold doesn't like your choice, but he respects it. He takes you to the future and you end up becoming a general that fights robots. But they don't exist because you didn't invent them. You are then stuck in a paradox and live in immesurable pain.")
             elif tea == "green":
                 print("Arnold really hates your choice. But you did let him in for tea, so you just get put in prison for your life and you don't know why. But, you are happy(somehow)")
         else:
@@ -48,12 +48,12 @@ else:
     else:
         terminating = True
 if terminating == True:
-    print("Thats too bad! The terminator comes in and eliminates you becuase you are the one that invents robots in the the future and ruins humanity. Sorry.")
+    print("Thats too bad! The terminator comes in and eliminates you because you are the one that invents robots in the the future and ruins humanity. Sorry.")
 if bankrob == True:
-    print("While you are robbing the bank, a cop comes in to deposit some moeny and arrests you.")
+    print("While you are robbing the bank, a cop comes in to deposit some money and arrests you.")
     if input("Do you try to break out of jail?(yes/no)\n").lower() == "yes":
         print("Somehow, you meet a really handy cellmate and together you break out and move to Rwanda so the government can't track you and live together happily ever after.")
     else:
         print("You live out the rest of your years in jail, until you die, and then you go to hell and are tortured for eternity.")
 if nobankrob == True:
-    print("Becuase you decided to not rob the bank, and angel comes down and blesses you with a good life as you are a good person at heart.")
+    print("Because you decided to not rob the bank, and angel comes down and blesses you with a good life as you are a good person at heart.")
